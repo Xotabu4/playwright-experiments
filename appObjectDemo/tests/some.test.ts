8fbcab0c43c6e286a87c044d9b56e382e013ed9b
Oleksandr Khotemskyi <xotabu4@gmail.com>
2022-02-02 18:04:54 +0200

    commit

--- appObjectDemo/tests/some.test.ts
+++ appObjectDemo/tests/some.test.ts
@@ -1,11 +1,16 @@
 import { test, expect } from '@playwright/test';
 import { App } from '../pages/app';
 
-test('example test', async ({ page }) => {
-  const app = App(page);
 
+const extendedTest = test.extend<{ app: ReturnType<typeof App> }>({
+  app: async ({ page }, use) => {
+    await use(App(page));
+  }
+})
+
+extendedTest('example test', async ({ page, app }) => {
   await app.login.open();
   await app.login.doLogin('test@test.com');
 
-  await expect(await page.locator('div').innerText()).toHaveText('logged in');
+  await expect(await page.locator('div')).toHaveText('logged in');
 });
--- tests/some.test.ts
+++ tests/some.test.ts
@@ -11,5 +11,21 @@ test('basic test', async ({ page }) => {
     await page.goto('https://playwright.dev/');
     const title = page.locator('.navbar__inner .navbar__title');
     await expect(title).toHaveText('Playwright');
+
+
+
+
+    const gameId = 123
+    const requestUrl = 'requrl.com'
+    
+    const url = page.evaluate(async ({ gameId, requestUrl }) => {
+      return fetch(requestUrl)
+        .then((r) => r.json())
+        .then((d) => {
+          const result = d.games.filter((item) => item.gameId === gameId);
+          return result.map((item) => item.jsPublicUrl).join('');
+        });
+    }, { gameId, requestUrl} );
+
   });
 })
